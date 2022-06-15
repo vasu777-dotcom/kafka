72ecf3e5d038f532d45ebad98287a8a9a3c8e8b6
vasu777-dotcom <57005318+vasu777-dotcom@users.noreply.github.com>
2022-06-15 16:52:01 +0530

    Update KafkaConsumerApplication.java

--- KafkaConsumer/src/main/java/com/example/consumerapp/KafkaConsumerApplication.java
+++ KafkaConsumer/src/main/java/com/example/consumerapp/KafkaConsumerApplication.java
@@ -9,6 +9,7 @@ public class KafkaConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 		System.out.println("Hello world");
+		System.out.println("ADDED THIS LINE IN GIT AND THEN WILL TRY TO PULL IT");
 	}
 	
 
